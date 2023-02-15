fac8bf4f822fed64e2157b5c9d2fde3d42b0d239
oliver-macdonald <125501081+oliver-macdonald@users.noreply.github.com>
2023-02-15 19:25:05 +0000

    Update printing.py
    added ingrediants sorted by frist letters starting with e

--- printing.py
+++ printing.py
@@ -1,12 +1,15 @@
 doggyNames=["daisy","teddy","velma","vector"]
 humanNames=["daisy","teddy","victor","vector","Victorea"]
+ingrediants=["flour", "eggs", "salt"]
 
 def listFilter(filterList,filterBy):
     for listItem in filterList:
         if listItem[0].lower() ==filterBy:
             print(listItem)
 
-
+print("filter by e on ingrediants" )         
+listFilter(ingrediants,"e")
+print("______________________________")
 print("filter by v")         
 listFilter(doggyNames,"v")
 print("______________________________")
